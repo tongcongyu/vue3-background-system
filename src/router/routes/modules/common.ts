6e531f34b1de3e81fb5a6d1c987dc44270492c66
H3C\tys4483 <YS.tongcongyu@h3c.com>
2023-04-12 10:38:25 +0800

    feat: 修改框架

--- src/router/routes/modules/common.ts
+++ src/router/routes/modules/common.ts
@@ -2,7 +2,7 @@
  * @Author: H3C\tys4483 YS.tongcongyu@h3c.com
  * @Date: 2023-01-18 17:12:21
  * @LastEditors: H3C\tys4483 YS.tongcongyu@h3c.com
- * @LastEditTime: 2023-02-01 13:49:24
+ * @LastEditTime: 2023-02-06 16:00:22
  * @FilePath: \vue3-background-system\src\router\routes\modules\common.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,6 +19,11 @@ const routes: RouteRecordRaw[] = [
     path: '/login',
     name: 'login',
     component: () => import('@/views/login/login.vue'),
+  },
+  {
+    path: '/ceshi',
+    name: 'ceshi',
+    component: () => import('@/views/ceshi/ceshi.vue'),
   },
   {
     path: '/main',
