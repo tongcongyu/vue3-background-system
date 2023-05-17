7cd591379fc3047b9abbbb4ab237453c3fd7cec8
H3C\tys4483 <YS.tongcongyu@h3c.com>
2023-05-17 15:13:45 +0800

    feat: 修改路由

--- src/router/routes/modules/common.ts
+++ src/router/routes/modules/common.ts
@@ -2,7 +2,7 @@
  * @Author: H3C\tys4483 YS.tongcongyu@h3c.com
  * @Date: 2023-01-18 17:12:21
  * @LastEditors: H3C\tys4483 YS.tongcongyu@h3c.com
- * @LastEditTime: 2023-02-06 16:00:22
+ * @LastEditTime: 2023-05-17 15:12:15
  * @FilePath: \vue3-background-system\src\router\routes\modules\common.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,13 +13,14 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: '',
-    redirect: '/login',
-  },
-  {
-    path: '/login',
-    name: 'login',
+    // redirect: '/login',
     component: () => import('@/views/mainIndex/index.vue'),
   },
+  //   {
+  //     path: '/login',
+  //     name: 'login',
+  //     component: () => import('@/views/mainIndex/index.vue'),
+  //   },
 ];
 
 export default routes;
